facd47d2a28953035a66c8e7e7133885e081275c
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2025-08-25 23:56:54 -0700

    add simple test dag

---
+++ dags/test.py
@@ -0,0 +1,12 @@
+from airflow.decorators import dag, task
+
+@dag(
+    schedule=None
+)
+def test_dag():
+    @task
+    def test_task():
+        print("Hello World")
+    test_task()
+
+test_dag()
